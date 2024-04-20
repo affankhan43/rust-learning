821c29ab6a9c6a5eeee13cb110e2481433e018cd
affankhan43 <affankhan627@gmail.com>
2024-04-20 21:27:28 +0200

    cargo_dir

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d
@@ -0,0 +1,5 @@
+/Users/affanahmedkhan/Projects/rust-learning/hello_world/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03: src/main.rs
+
+/Users/affanahmedkhan/Projects/rust-learning/hello_world/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/affanahmedkhan/Projects/rust-learning/hello_world/hello_cargo/target/debug/hello_cargo: /Users/affanahmedkhan/Projects/rust-learning/hello_world/hello_cargo/src/main.rs
