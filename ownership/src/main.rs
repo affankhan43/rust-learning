8b3aa6b7f22a73b08962e0a0f61ed5bdd0aa95fd
affankhan43 <affankhan627@gmail.com>
2024-05-02 15:51:36 +0200

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    
+
+    //ownership rules:
+    // 1. Each value in RUST has a variable that called it's owner.
+    // 2. There will be only one owner at a time.
+    // 3. When the owner goes out of the scope,  the value will be dropped.
 }
